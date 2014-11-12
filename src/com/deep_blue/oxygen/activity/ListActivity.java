16a2e4ce8f7a7ffad3da6656f58afe7a3bc858a5
cgstag <flavio.deroo@gmail.com>
2014-11-12 15:40:00 +0100

    Layout List FIche / Ressources
    Création du layout liste fiche et des ressources associés String &
    Drawable

---
+++ src/com/deep_blue/oxygen/activity/ListActivity.java
@@ -0,0 +1,27 @@
+package com.deep_blue.oxygen.activity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
+
+import com.deep_blue.oxygen.R;
+import com.deep_blue.oxygen.dao.FicheSecuriteDao;
+import com.deep_blue.oxygen.dao.UtilisateurDao;
+import com.deep_blue.oxygen.model.FicheSecurite;
+
+public class ListActivity extends Activity {
+
+    
+    
+}
