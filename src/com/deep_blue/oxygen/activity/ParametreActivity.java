e954192fce825cac28c8252b9426f058b4ce081b
raphael <raphael.bideau@gmail.com>
2014-12-15 22:08:58 +0100

    Ajout de paramètres
    Récupération uniquement des fiches perso ou de toute les fiches
    Nombre de jours de fiche récupéré

--- src/com/deep_blue/oxygen/activity/ParametreActivity.java
+++ src/com/deep_blue/oxygen/activity/ParametreActivity.java
@@ -9,6 +9,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,11 +38,57 @@ public class ParametreActivity extends Activity {
 				.setChecked(preferences.getBoolean(
 						PreferenceKey.SAVE_LOGIN.toString(), false));
 		((CheckBox) findViewById(R.id.checkBox_keep_connection))
-		.setChecked(preferences.getBoolean(
-				PreferenceKey.KEEP_CONNECTION.toString(), false));
+				.setChecked(preferences.getBoolean(
+						PreferenceKey.KEEP_CONNECTION.toString(), false));
 		((TextView) findViewById(R.id.textView_parametre_last_synch_value))
 				.setText(preferences.getString(
 						PreferenceKey.LAST_SYNCH.toString(), "jamais"));
+
+		if (preferences.getBoolean(
+				PreferenceKey.RETREIVE_TYPE_ALL_FICHE.toString(), true))
+			((RadioGroup) findViewById(R.id.radioGroup_retreive_type))
+					.check(R.id.radio_parametre_retrieve_type_all);
+		else
+			((RadioGroup) findViewById(R.id.radioGroup_retreive_type))
+					.check(R.id.radio_parametre_retrieve_type_only_own);
+
+		int retrieveLength = preferences.getInt(
+				PreferenceKey.RETREIVE_LENGTH.toString(), 2);
+		final TextView textViewRetrieveLength = (TextView) findViewById(R.id.textView_parametre_retrieve_length_label);
+		if (retrieveLength > 0)
+			textViewRetrieveLength.setText(getResources().getQuantityString(
+					R.plurals.parametre_retrieve_length_label, retrieveLength,
+					retrieveLength));
+		else
+			textViewRetrieveLength
+					.setText(R.string.parametre_retrieve_length_label_zero);
+		SeekBar seekBarRetrieveLength = (SeekBar) findViewById(R.id.seekBar_retrieve_length);
+		seekBarRetrieveLength.setProgress(retrieveLength);
+		seekBarRetrieveLength
+				.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+					@Override
+					public void onStopTrackingTouch(SeekBar seekBar) {
+					}
+
+					@Override
+					public void onStartTrackingTouch(SeekBar seekBar) {
+					}
+
+					@Override
+					public void onProgressChanged(SeekBar seekBar,
+							int progress, boolean fromUser) {
+						if (progress > 0)
+							textViewRetrieveLength
+									.setText(getResources()
+											.getQuantityString(
+													R.plurals.parametre_retrieve_length_label,
+													progress, progress));
+						else
+							textViewRetrieveLength
+									.setText(R.string.parametre_retrieve_length_label_zero);
+					}
+				});
+
 	}
 
 	@Override
@@ -72,30 +121,45 @@ public class ParametreActivity extends Activity {
 				.getDefaultSharedPreferences(this);
 		SharedPreferences.Editor pEditor = preferences.edit();
 
-		
-		boolean checkedSaveLogin = ((CheckBox) findViewById(R.id.checkBox_save_login)).isChecked();
-		if(!checkedSaveLogin){
+		boolean checkedSaveLogin = ((CheckBox) findViewById(R.id.checkBox_save_login))
+				.isChecked();
+		if (!checkedSaveLogin) {
 			pEditor.remove(PreferenceKey.SAVED_LOGIN.toString());
 		}
-		pEditor.putBoolean(PreferenceKey.SAVE_LOGIN.toString(),checkedSaveLogin);
-		
-		boolean checkedKeepConnection = ((CheckBox) findViewById(R.id.checkBox_save_login)).isChecked();
-		if(!checkedKeepConnection){
+		pEditor.putBoolean(PreferenceKey.SAVE_LOGIN.toString(),
+				checkedSaveLogin);
+
+		boolean checkedKeepConnection = ((CheckBox) findViewById(R.id.checkBox_save_login))
+				.isChecked();
+		if (!checkedKeepConnection) {
 			pEditor.remove(PreferenceKey.KEPT_CONNECTION_LOGIN.toString());
 			pEditor.remove(PreferenceKey.KEPT_CONNECTION_VERSION.toString());
 		}
-		pEditor.putBoolean(PreferenceKey.KEEP_CONNECTION.toString(),checkedKeepConnection);
-		
-		
+		pEditor.putBoolean(PreferenceKey.KEEP_CONNECTION.toString(),
+				checkedKeepConnection);
+
 		pEditor.putString(PreferenceKey.REMOTE_URL.toString(),
 				((EditText) findViewById(R.id.editText_parametre_remote_url))
 						.getText().toString());
-		
+
 		pEditor.putString(PreferenceKey.LAST_SYNCH.toString(), preferences
 				.getString(PreferenceKey.LAST_SYNCH.toString(), "jamais"));
 
+		if (((RadioGroup) findViewById(R.id.radioGroup_retreive_type))
+				.getCheckedRadioButtonId() == R.id.radio_parametre_retrieve_type_all) {
+			pEditor.putBoolean(
+					PreferenceKey.RETREIVE_TYPE_ALL_FICHE.toString(), true);
+		} else {
+			pEditor.putBoolean(
+					PreferenceKey.RETREIVE_TYPE_ALL_FICHE.toString(), false);
+		}
+
+		pEditor.putInt(PreferenceKey.RETREIVE_LENGTH.toString(),
+				((SeekBar) findViewById(R.id.seekBar_retrieve_length))
+						.getProgress());
+
 		pEditor.apply();
-		
+
 		Toast toast = Toast.makeText(this, R.string.parametre_saved,
 				Toast.LENGTH_SHORT);
 		toast.show();
